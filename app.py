050f84808f466befc9d34305753bba59064f085b
Shamroz Abrar <shamrozahmad2412@gmail.com>
2025-12-22 14:17:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pickle
 import numpy as np
 
-model = pickle.load(open("backend/rf_model.pkl", "rb"))
-scaler = pickle.load(open("backend/scaler.pkl", "rb"))
+model = pickle.load(open("rf_model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
 st.title("HPV Epitope Scan")
 st.write("Demo app placeholder. Full features pending.")
